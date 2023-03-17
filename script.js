51a449a7482f523e0b8e7abbf498ebe8d1281f06
Ramya Sree <ramyasree@Ramyas-Laptop.local>
2023-03-16 23:47:19 -0500

    Working on Dom

--- script.js
+++ script.js
@@ -17,32 +17,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  let mainEl=document.getElementById('main-title');
+  mainEl.textContent="Welcome to  homepage";
 
   // Part 2
 
+  let bdy=document.querySelector("body");
+  bdy.style.backgroundColor="green";
+
 
   // Part 3
-
+  let favlist=document.getElementById("favorite-things");
+  favlist.removeChild(favlist.lastElementChild);
 
   // Part 4
-
-
+  
+ 
   // Part 5
-
-
+  let pastRac=document.getElementById("past-races")
+   let child=pastRac.childNodes[7]
+   child.parentNode.removeChild(child);
+  
   // Part 6
 
-
+  pastRac.innerHTML +=`<il>Indiana</iL>`
   // Part 7
+ const el=document.createElement("div")
+ el.classList.add("blog-post");
+ el.innerHTML +=`<h2>HI</h2> <p>hi this ramya welcome<p>`
 
+document.body.appendChild(el);
 
   // Part 8
 
+const sel=document.querySelector("#quote-title")
+sel.addEventListener("click", e => {
+e.randomQuote();
+
+});
 
   // Part 9
 
+let blgpst=document.querySelectorAll("blog-post")
+console.log(blgpst);
+blgpst.forEach( (blg) =>{
+  console.log(blg);
 
-
+blg.addEventListener("mouseout", e=>{
+  
+  e.classList.toggle("purple")
+  
+});
+blg.addEventListener("mouseenter", e=>{
+ e.classList.toggle("red");
+});
+});
 
 });
+
